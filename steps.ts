7a809dc247f3581c2c4dfbb9802c24c4ca974372
jdraper <jdraper@wcf.com>
2019-02-08 16:25:23 -0700

    first commit

---
+++ steps.ts
@@ -0,0 +1,10 @@
+/* Defines the product entity */
+export class Steps {
+    value: string;
+    dateTime: string;
+     constructor(value: string, dateTime: string) {
+        this.value = value;
+        this.dateTime = dateTime;
+    }
+}
+
